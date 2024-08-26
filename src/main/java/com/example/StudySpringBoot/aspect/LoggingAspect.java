05e84a5464183941f1cc865df86eb4866e105d92
Chang-Young Park <parkcy0630@gmail.com>
2024-08-26 10:27:07 +0900

    240826 - AOP(핵심 관심 사항과 공통 관심 사항을 분리) - 공통 관심 사항 예시(로깅 / 보안 / 예외 처리 등)

---
+++ src/main/java/com/example/StudySpringBoot/aspect/LoggingAspect.java
@@ -0,0 +1,32 @@
+package com.example.StudySpringBoot.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.example.StudySpringBoot.controller.PlayerController.*(..))")
+    public void logBeforeMethod(JoinPoint joinPoint) {
+        logger.info("LoggingAspect - Executing method: " + joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning(value = "execution(* com.example.StudySpringBoot.controller.PlayerController.*(..))", returning = "result")
+    public void logAfterMethod(JoinPoint joinPoint, Object result) {
+        logger.info("LoggingAspect - Method executed: " + joinPoint.getSignature().getName() + ", Result: " + result);
+    }
+
+    @AfterThrowing(value = "execution(* com.example.StudySpringBoot.controller.PlayerController.*(..))", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Exception exception) {
+        logger.error("LoggingAspect - Exception in method: " + joinPoint.getSignature().getName() + ", Exception: " + exception.getMessage());
+    }
+}
