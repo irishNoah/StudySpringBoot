7af559cb36e2c7d966ef2da239a1a0007e66d30b
Chang-Young Park <parkcy0630@gmail.com>
2024-08-28 15:53:57 +0900

    240826 - 불필요 Entity 및 관련 파일 삭제

--- src/main/java/com/example/StudySpringBoot/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.StudySpringBoot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.StudySpringBoot.entity.Player;
-
-public interface PlayerRepository extends JpaRepository<Player, String> {
-}
--- src/main/java/com/example/StudySpringBoot/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.StudySpringBoot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.StudySpringBoot.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
